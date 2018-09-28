e2e58aa5acebd401f1ee4283573e55544b94620f
Zaheer Jan <zaheerjan0@gmail.com>
2018-09-28 15:04:54 +0500

    new models admin

--- restaurant_stocks/admin.py
+++ restaurant_stocks/admin.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-from restaurant_stocks.models import Stock, StockIn, StockOut
+from restaurant_stocks.models import (
+    Stock, StockIn, StockOut,
+    StockClosed, StockItemClosed
+)
 
 
 class StockAdmin(admin.ModelAdmin):
@@ -31,6 +35,26 @@ class StockOutAdmin(admin.ModelAdmin):
     )
 
 
+class StockClosedAdmin(admin.ModelAdmin):
+    list_display = (
+        '__unicode__', 'closed_stock_in', 'closed_stock_out',
+        'closed_stock_amount', 'remaining_stock'
+    )
+    list_filter = (('closing_date', DateFieldListFilter),)
+
+
+class ClosedItemAdmin(admin.ModelAdmin):
+    list_display = (
+        '__unicode__', 'stock_closed', 'item_stock_in', 'item_stock_out',
+        'closed_stock_amount', 'remaining_stock_item'
+    )
+    search_fields = ('stock__item_name',)
+    list_filter = (('stock_closed__closing_date', DateFieldListFilter),)
+    raw_id_fields = ('stock', 'stock_closed',)
+
+
 admin.site.register(Stock, StockAdmin)
 admin.site.register(StockIn, StockInAdmin)
 admin.site.register(StockOut, StockOutAdmin)
+admin.site.register(StockClosed, StockClosedAdmin)
+admin.site.register(StockItemClosed, ClosedItemAdmin)
